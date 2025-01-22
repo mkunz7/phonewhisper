f00f09fc1efe2fe8695927d3d9cb8565496477ac
mkunz7 <mkunz7@users.noreply.github.com>
2025-01-22 17:12:08 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+mkdir -p /usr/share/voice-server
+mkdir -p /usr/share/voice-server/public
+
+ARCH=$(uname -m)
+if echo "$ARCH" | grep -q "mips"; then
+    echo "System is running on MIPS."
+    curl https://github.com/mkunz7/phonewhisper/releases/download/main/voice-server-linux-mips -o /usr/share/voice-server/voiceserver
+
+# Check for ARMv7
+elif echo "$ARCH" | grep -q "armv7"; then
+    echo "System is running on ARMv7."
+    curl https://github.com/mkunz7/phonewhisper/releases/download/main/voice-server-linux-arm7 -o /usr/share/voice-server/voiceserver
+
+else
+    echo "Architecture not currently supported by installer: $ARCH"
+    exit 1
+fi
+
+curl https://raw.githubusercontent.com/mkunz7/phonewhisper/refs/heads/main/voice-server -o /etc/init.d/voice-server
+curl https://raw.githubusercontent.com/mkunz7/phonewhisper/refs/heads/main/public/listen.html -o /usr/share/voice-server/public/listen.html
+curl https://raw.githubusercontent.com/mkunz7/phonewhisper/refs/heads/main/public/broadcast.html -o /usr/share/voice-server/public/broadcast.html
+
+chmod +x /usr/share/voice-server/voiceserver
+chmod +x /etc/init.d/voice-server
+
+/etc/init.d/voice-server start
+/etc/init.d/voice-server enable
+echo "done"
