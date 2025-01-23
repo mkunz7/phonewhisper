d9ba5f88fe5692533a34da36aa88b415d3875ea1
mkunz7 <mkunz7@users.noreply.github.com>
2025-01-22 18:42:03 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags="-w -s" -o voice-server-linux-arm7
+CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -o voice-server-linux-mipsle
 CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -o voice-server-linux-mips
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-w -s" -o voice-server-linux-x86
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o voice-server-linux-x64
